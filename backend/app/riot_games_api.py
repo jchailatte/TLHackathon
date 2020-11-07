f99c37197e409de14dc6dca9afb75c29971cb592
Russell Wakugawa <wakugawa@usc.edu>
2020-11-06 22:42:56 -0800

    pull riot api key from config

--- backend/app/riot_games_api.py
+++ backend/app/riot_games_api.py
@@ -1,12 +1,12 @@
 
 import requests
+from config import RIOT_API_KEY
 
 # To regenerate: https://developer.riotgames.com/
 # Expires: Sat, Nov 7th, 2020 @ 7:35pm (PT) in 21 hours and 26 minutes
 # 20 requests every 1 seconds(s)
 # 100 requests every 2 minutes(s)
-RIOT_GAMES_API = "RGAPI-fcccb2ad-d44d-4b63-bd22-a03af410a87b"
 
 def lol_api_request(endpoint):
   url = "https://na1.api.riotgames.com/" + endpoint
-  return requests.get(url, headers={'X-Riot-Token': RIOT_GAMES_API}).json()
+  return requests.get(url, headers={'X-Riot-Token': RIOT_API_KEY}).json()
