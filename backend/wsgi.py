6a077b33dc7474115bfd0d90440d6264c85e3fec
Russell Wakugawa <wakugawa@usc.edu>
2020-11-06 20:49:15 -0800

    correcting app name

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -3,4 +3,4 @@ from app import app
 
 if __name__ == '__main__':
     print('Serving on 8088...')
-    WSGIServer(('127.0.0.1', 8088), application).serve_forever()
+    WSGIServer(('127.0.0.1', 8088), app).serve_forever()
