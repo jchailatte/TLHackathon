63618fddb330c0e49c325ae644a044d0e017463b
Kevin Jin <kevin.jin@vanderbilt.edu>
2020-11-07 00:34:04 -0600

    nice

--- backend/app/match_history.py
+++ backend/app/match_history.py
@@ -2,11 +2,6 @@ import json
 import requests
 from riot_games_api import lol_api_request
 
-# Expires: Sat, Nov 7th, 2020
-# RATE LIMITS
-# 20 requests every 1 seconds(s)
-# 100 requests every 2 minutes(s)
-RIOT_GAMES_API = "RGAPI-fcccb2ad-d44d-4b63-bd22-a03af410a87b"
 
 def get_match_history(player_name):
   account_data = lol_api_request("lol/summoner/v4/summoners/by-name/" + player_name)
@@ -16,7 +11,11 @@ def get_match_history(player_name):
   for match in match_history[:15]:
     match_id = match["gameId"]
     match_details = lol_api_request("lol/match/v4/matches/" + str(match_id))
-    match_details["is_player"] = True
+    for match_player in match_details["participantIdentities"]:
+      if match_player["player"]["accountId"] == account_id:
+        match_player["player"]["is_current_player"] = True
+        # only one of the players should match per game, so break after found.
+        break
     expanded_match_history.append(match_details)
 
   return json.dumps(expanded_match_history)
